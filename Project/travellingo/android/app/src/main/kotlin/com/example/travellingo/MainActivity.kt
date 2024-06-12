b0d05c1ebdecb47d4a64d7a8d6e0cf742eeb23ee
sammmms <samuelonasis23@gmail.com>
2024-06-12 10:24:17 +0700

    chore: remove

--- Project/travellingo/android/app/src/main/kotlin/com/example/travellingo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travellingo
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity()
--- Project/travellingo/lib/utils/locales/locale.dart
+++
@@ -1,7 +0,0 @@
-import "id.dart";
-import "us.dart";
-
-mixin AppLocale {
-  static const Map<String, dynamic> en = usLanguage;
-  static const Map<String, dynamic> id = idLanguage;
-}
